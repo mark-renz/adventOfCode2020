dbaea9221162f4e1c24e9ec4ea32ba2444f1dfaa
mark-renz <markrenzmanalo@gmail.com>
2020-12-11 20:57:02 +0800

    day1 fixed finished

--- Day1/solver.js
+++ Day1/solver.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
+
 fs.readFile('input.txt', 'utf8', (err, data)=>{
     if(err) throw err;
 
@@ -8,13 +9,38 @@ fs.readFile('input.txt', 'utf8', (err, data)=>{
     let arrayOfNumbers = dataToArray.map(Number);
 
     console.log(arrayOfNumbers);
+    productOfTwo(arrayOfNumbers);
+    productOfThree(arrayOfNumbers);
 
+});
+
+
+function productOfTwo(arrayOfNumbers){
     for (let i = 0; i<arrayOfNumbers.length;i++){
-        let comp = 2020 - arrayOfNumbers[i];
-        if(arrayOfNumbers.includes(comp)){
-            let answer = comp * arrayOfNumbers[i];
-            console.log(`answer: ${answer}`);
-            break;
+    let comp = 2020 - arrayOfNumbers[i];
+    if(arrayOfNumbers.includes(comp)){
+        let answer = comp * arrayOfNumbers[i];
+        console.log(`answer 2 Numbers: ${answer}`);
+        break;
+    }
+    }
+}
+
+function productOfThree(arrayOfNumbers){
+    
+    let check = false;
+    for(let i =0; i<arrayOfNumbers.length-2; i++){
+        let sumOfTwo = 2020 - arrayOfNumbers[i];
+        for(let j=i+1; j<arrayOfNumbers.length;j++){
+            let sumOfThree = sumOfTwo - arrayOfNumbers[j];
+            if(arrayOfNumbers.includes(sumOfThree)){
+                console.log(`${arrayOfNumbers[i]},${sumOfThree},${arrayOfNumbers[j]}`)
+                let answer = arrayOfNumbers[i] *sumOfThree* arrayOfNumbers[j];
+                console.log(`answer 3 Numbers: ${answer}`);
+                check =true;
+                break;
+            }
+            if(check) break;
         }
     }
-})
+}
