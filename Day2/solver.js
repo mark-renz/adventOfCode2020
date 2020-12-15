2447b325063410bdb3cadd59ff3a1d62aed6e72f
mark-renz <markrenzmanalo@gmail.com>
2020-12-15 23:34:42 +0800

    added part 2 to solution wrong answer

--- Day2/solver.js
+++ Day2/solver.js
@@ -3,11 +3,7 @@ var fs = require('fs');
 let validCount = 0;
 let items = [];
 
-fs.readFile('input.txt', 'utf8', (err, data)=>{
-    if(err) throw err;
-    splitData(data);
-    
-});
+main();
 
 function splitData(data){
     //console.log(data);
@@ -20,37 +16,62 @@ function splitData(data){
         let password = item.match(/[^\s]+$/);
 
         let obj ={'lowerLimit':lowerLimit[0],'upperLimit':upperLimit[0],'letter':letter[0],'password':password[0]};
-        items.push(obj);
-        //console.log(items);
-        //console.log(`lowerlimit: ${lowerLimit} upperlimit: ${upperLimit} letter: ${letter} password: ${password}`)
-       //checkPassword(lowerLimit[0],upperLimit[0],letter[0],password[0]);
+      items.push(obj);
+        /*console.log(items);
+        console.log(`lowerlimit: ${lowerLimit} upperlimit: ${upperLimit} letter: ${letter} password: ${password}`)
+       checkPassword(lowerLimit[0],upperLimit[0],letter[0],password[0]);
 
-        //let upperlimit = item.match(//);
-        // let lowerLimit = item.split('-');
-        // let upperLimit = lowerLimit[1].split(" ");
-        // let letter = upperLimit[1].split(':');
-        // let password = upperLimit[2].split(',');
-        // console.log(`${lowerLimit[0]} ${upperLimit[0]} ${letter[0]} ${password}`);
+        let upperlimit = item.match(//);
+        let lowerLimit = item.split('-');
+        let upperLimit = lowerLimit[1].split(" ");
+        let letter = upperLimit[1].split(':');
+        let password = upperLimit[2].split(',');
+        console.log(`${lowerLimit[0]} ${upperLimit[0]} ${letter[0]} ${password}`); */
         
     });
-
-    function checkPassword(lowerLimit, upperLimit, letter, password ){
-        let regex = new RegExp(letter,'g');
-        let count = (password.match(regex) || []).length;
-        if(parseInt(lowerLimit)<=count && parseInt(upperLimit)>=count){
-           return true;
-        }
-        else false;
-
-    }
-
-    items.map((item)=>{
-        let {lowerLimit,upperLimit,letter,password} = item;
-        if(checkPassword(lowerLimit, upperLimit, letter,password)){
-            validCount++;
-        }
-    })
-
-    console.log(validCount);
-
 }
+    function checkPassword(lowerLimit, upperLimit, letter, password, mode){
+        let regex = new RegExp(letter,'g');
+        let count = 0;
+        if(mode === 'a'){
+            count = (password.match(regex) || []).length;
+                if(parseInt(lowerLimit)<=count && parseInt(upperLimit)>=count){
+                    return true;
+                }
+                else false;
+            }
+        if(mode === 'b'){
+            let a = password.includes(letter,lowerLimit-1) 
+             let b = password.includes(letter,upperLimit-1)
+            if(a === !b){
+                console.log(`${password},${lowerLimit},${upperLimit},${letter},${a},${b}`);
+                return true;
+            }
+            else false;
+        }
+    }
+    
+    
+
+    function main(){
+    fs.readFile('input.txt', 'utf8', (err, data)=>{
+        if(err) throw err;
+        splitData(data);
+        items.map((item)=>{
+            let {lowerLimit,upperLimit,letter,password} = item;
+            if(checkPassword(lowerLimit, upperLimit, letter,password, 'a')){
+                validCount++;
+            }
+            
+        });
+    
+        console.log(validCount);
+         
+        });
+    }
+      
+
+
+
+
+
