fb1d80eceed505b9e0669eb2b8e54d4579bdc71a
mark-renz <markrenzmanalo@gmail.com>
2020-12-20 20:43:23 +0800

    day 4 part 2 finished

--- Day4/solver.js
+++ Day4/solver.js
@@ -5,9 +5,19 @@ const { fileURLToPath } = require('url');
 const data = fs.readFileSync('input.txt', 'utf8').toString().split(/\r\n/);
 
 const passports = getPassports(data);
-const count = passports.filter(passport=>checkFields(passport)).length;
+const valid1 = passports.filter(passport=>checkFields(passport));
 
-console.log(`valid ids: ${count}`);
+const valid2 = valid1.filter(passport=>
+    checkByr(passport['byr']) 
+    && checkIyr(passport['iyr'])
+    && checkEyr(passport['eyr'])
+    && checkHgt(passport['hgt'])
+    && checkHcl(passport['hcl'])
+    && checkEcl(passport['ecl'])
+    && checkPid(passport['pid']));
+
+console.log(`valid ids part 1: ${valid1.length}`);
+console.log(`valid ids part 2: ${valid2.length}`);
 
 function checkFields(passport){
 required = ['byr','iyr','eyr','hgt','hcl','ecl','pid'];
@@ -20,13 +30,13 @@ function getPassports(data) {
     let ctr = 0;
     let passports =[];
     for(let i=0;i<data.length;i++){
+        temp+= " " +data[i];
+        //console.log(temp);
+
         if(data[i] === '' || i>=data.length-1){
             passports[ctr] = temp.trim();
             ctr++;
             temp = '';
-        }
-        else{
-            temp+= " " +data[i];
         }
     }
     const id = passports.map(passport=>{
@@ -38,4 +48,53 @@ function getPassports(data) {
     })
     return id;
 }
+
+function checkByr(input){
+    return (input>=1920 && input<=2002);
+}
+
+function checkIyr(input){
+    return (input>=2010 && input<=2020);
+    }
+
+function checkEyr(input){
+    return (input>=2020 && input<=2030);
+}
+
+function checkHgt(input){
+    if(input === undefined)
+        return false;
+    else{
+        height = input.split(/(\d+)/g);
+
+            if(height[2].trim() === 'cm'){
+                return (parseInt(height[1])>=150 && (parseInt(height[1])<=193))
+            }
+            else if(height[2].trim() === 'in'){
+                return (parseInt(height[1])>=59 && (parseInt(height[1])<=76))
+            }
+            else return false;
+        }
+}
+
+function checkHcl(input){
+    const regex = RegExp(/#[0-9a-f]{6}/);
+    return regex.test(input);
+}
+
+function checkEcl(input){
+    const color = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+    return color.includes(input);
+}
+
+function checkPid(input){
+    return !isNaN(input) && input.length === 9;
+}
+
+
+
+
+
+
+
 
