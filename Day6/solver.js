c4eb7ca4ff57bde24f6c3a89fdca44a05078c1b3
mark-renz <markrenzmanalo@gmail.com>
2020-12-22 22:54:23 +0800

    day 6 part 2 finished

--- Day6/solver.js
+++ Day6/solver.js
@@ -1,14 +1,43 @@
 var fs = require('fs');
 
 const data = fs.readFileSync('input.txt', 'utf8').toString().split(/\r\n\r\n/);
-
-let count = 0;
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-data.map(item=>{
-    alphabet.map((letter)=>{
+
+const count = countYes(data,alphabet);
+console.log(`total yes 1: ${count}`);
+
+const count2 = countYes2(data,alphabet);
+console.log(`total yes 2: ${count2}`);
+
+
+function countYes(data,alphabet){
+let count = 0;
+data.forEach(item=>{
+    alphabet.forEach((letter)=>{
         if(item.includes(letter))
         count++;
     })
 });
+return count;
+}
 
-console.log(`total yes: ${count}`);
+function countYes2(data,alphabet){
+    let count = 0;
+    const truthy = new Array(alphabet.length); 
+    data.map(items=>{
+        item = items.split('\r\n');
+        truthy.fill(0,0,26);
+        item.forEach(ans =>{
+            alphabet.forEach((letter,index)=>{
+                if(ans.includes(letter)){
+                truthy[index]++;
+                if(truthy[index] === item.length){
+                            count++;
+                        }     
+                    }
+                });
+            });
+        });
+
+    return count;
+}
